4f04fabb4ab01546782d761762a9271cc1e2dcfc
Michael Harrington <mharrington@cayan.com>
2018-10-22 14:26:38 -0400

    Added basic copyright information into the assembly.

--- src/TcpStumps.Base/Properties/AssemblyInfo.cs
+++ src/TcpStumps.Base/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TcpStumps.Base")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("TCP-Stumps Runtime")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TcpStumps.Base")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Cayan, LLC")]
+[assembly: AssemblyProduct("TCP-Stumps")]
+[assembly: AssemblyCopyright("Copyright © 2018 Cayan, LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
